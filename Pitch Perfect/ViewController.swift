fc91bdb1228763ef28f0b1b452a88cfa8a89c9f0
Greg Palen <codingvirtual@gmail.com>
2015-05-13 20:22:04 -0500

    initial commit - forgot to do this along the way. Have a record button, a "recording" label, and a stop button and some interaction between them. Just added a NavController and am about to add a 2nd screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var stopRecording: UIButton!
+    
+    @IBOutlet weak var startRecording: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecording.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false
+        stopRecording.hidden = false
+        startRecording.enabled = false
+        //TODO: Record the user's voice
+        println("in recordAudio")
+    }
+
+    @IBAction func stopAudio(sender: UIButton) {
+        recordingInProgress.hidden = true
+        stopRecording.hidden = true
+        startRecording.enabled = true
+        //TODO: stop recording audio
+    }
 
 }
 
